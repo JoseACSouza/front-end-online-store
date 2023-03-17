cb9ff2e0174b3908e2574ed146bffc1386640635
lucas sanfelice <115578920+Lucas-Sanfelice@users.noreply.github.com>
2023-03-17 18:26:46 -0300

    commit

---
+++ src/components/CategoriesList.jsx
@@ -0,0 +1,31 @@
+import React, { Component, useState, useEffect } from 'react';
+import { PropTypes } from 'prop-types';
+import { getCategories } from '../services/api';
+
+class CategoriesList extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      categories: [],
+    };
+  }
+
+  async componentDidMount() {
+    const categories = await getCategories();
+    this.setState({ categories });
+  }
+
+  render() {
+    const { categories } = this.state;
+    return (
+      <div>
+        {categories.map((categoria) => (
+          <button data-testid="category" htmlFor="" key={ categoria.id }>
+            {categoria.id}
+          </button>
+        ))}
+      </div>
+    );
+  }
+}
+export default CategoriesList;
