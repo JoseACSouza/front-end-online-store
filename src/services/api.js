4cc2d0fb34ffb2a2c572f7d705021fde9d012380
lucas sanfelice <115578920+Lucas-Sanfelice@users.noreply.github.com>
2023-03-17 16:11:50 -0300

    main-group-7-funções-fetch

--- src/services/api.js
+++ src/services/api.js
@@ -1,12 +1,22 @@
 export async function getCategories() {
   // Implemente aqui
+  const fetCategorys = await fetch('https://api.mercadolibre.com/sites/MLB/categories');
+  return fetCategorys;
 }
-
-export async function getProductsFromCategoryAndQuery(/* categoryId, query */) {
+console.log(getCategories());
+export async function getProductsFromCategoryAndQuery(CATEGORY_ID, QUERY) {
+  const fetCategory = await (await fetch(`https://api.mercadolibre.com/sites/MLB/search?category=${CATEGORY_ID}`)).json();
+  const fetQuery = await (await fetch(`https://api.mercadolibre.com/sites/MLB/search?category=${QUERY}`)).json();
   // Implemente aqui! Quando o fizer, descomente os parâmetros que essa função recebe
+  return {
+    fetCategory,
+    fetQuery,
+  };
 }
 
 export async function getProductById() {
+  const fetQuery = await (await fetch(`https://api.mercadolibre.com/sites/MLB/search?category=${QUERY}`)).json();
+  return fetQuery;
   // Esta implementação específica não é avaliada, mas pode ajudar você 🙂
   // Atenção: essa função não deverá ser chamada na tela do carrinho de compras.
 }
