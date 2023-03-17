9fd1256e7349c83138638c207c0650cb9cbf7717
José Alexandre Cordeiro Souza <alexandrecordeiro15@outlook.com>
2023-03-17 17:08:01 -0300

    adiciona a pagina do carrinho de compras vazio

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { Route, Switch } from 'react-router-dom';
 import Home from './pages/Home';
+import ShoppingCart from './pages/ShoppingCart';
 
 function App() {
   return (
@@ -8,6 +9,7 @@ function App() {
       <Switch>
         {/* Quando estiver na rota / renderize: Login */}
         <Route exact path="/" component={ Home } />
+        <Route exact path="/shoppingCart" component={ ShoppingCart } />
       </Switch>
     </main>
   );
